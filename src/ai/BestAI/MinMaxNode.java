a6bcade5cdecba118ffc6e1b1a3ece347dcc8a12
Capelliexp <the.1man@hotmail.com>
2017-09-18 22:06:06 +0200

    sexdrive implemented

--- src/ai/BestAI/MinMaxNode.java
+++ src/ai/BestAI/MinMaxNode.java
@@ -1,37 +1,64 @@
 package ai.BestAI;
 
-import java.util.*;
-import java.util.LinkedList;
 import kalaha.*;
 
 public class MinMaxNode {
 	private GameState state;							//state of the virtual game
 	private int minMaxValue;							//value for how good each terminal state is
 	private int childNr;								//value of 1-6
+	//private int playerTurn;	 						//is not required, found in state
+	private boolean sexdrive;
 	private MinMaxNode parent;							//pointer to parent
 	private MinMaxNode[] children = new MinMaxNode[6];	//array of children
 	
-	public MinMaxNode(MinMaxNode parent, int childNr){
-		this.parent = parent;
-		this.childNr = childNr;
-		
-		//skapar en state som är samma som förälderns + 1/6 drag
-		this.state = parent.GetState().clone();
-		//this.state.makeMove(childNr);
-		Action(childNr);
-		
-		//if - to stop the sexdrive 
-		for(int i = 0; i > 6; i++)
-			children[i]  = new MinMaxNode(this, i);
-	}
+	//-------------------------------
 	
+	//Getters & Setters
 	public GameState GetState(){
 		return this.state;
 	}
 	
-	public boolean Action(int move){
-		boolean legal = state.makeMove(move);
-		//if not legal - stop sexdrive and fuck the minMaxValue
-		return legal;
+	//-------------------------------
+	
+	//Constructors
+	public MinMaxNode(MinMaxNode parent, int childNr){	//constructor as child
+		this.parent = parent;
+		this.childNr = childNr;
+		this.sexdrive = true;
+		
+		//skapar en state som är samma som förälderns + 1/6 drag
+		this.state = parent.GetState().clone();
+		Action(childNr);
+		
+		//if - to stop the sexdrive depending on tree depth
+		if(this.sexdrive == true){}
+		for(int i = 0; i > 6; i++)
+			children[i]  = new MinMaxNode(this, i+1);	//OBS! i+1
 	}
+	
+	public MinMaxNode(GameState trueState){	//constructor as root
+		this.state = trueState;	//this node represents the current game
+		this.childNr = -1;	//not a child
+	}
+	
+	//-------------------------------
+	
+	//Functions
+	public int Action(int move){
+		int returnValue = 1;
+		boolean legal = state.makeMove(move);	//move parameter is 1-6
+		
+		//if not legal - stop sexdrive and fuck the minMaxValue
+		if(legal == false){
+			this.sexdrive = false;
+			minMaxValue = -9999;	//temp - not a good solution, right?
+			returnValue = -1;
+		}
+		return returnValue;
+	}
+	
+	//public boolean TerminalTest(){}
+	
+	//public int UtilityFunction(){}
+
 }
